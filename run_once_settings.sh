138a38e7d2cba72d432de0cb255a3834b8394e8f
Benjamin Collet <benjamin.collet@protonmail.ch>
2021-10-14 10:44:38 +0200

    Remove afnor from vconsole to avoid error with 🄯

--- run_once_settings.sh
+++ run_once_settings.sh
@@ -12,7 +12,7 @@ gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 3700
 gsettings set org.gnome.settings-daemon.plugins.power power-button-action 'interactive'
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
-localectl set-keymap fr-bepo_afnor
+localectl set-keymap fr-bepo
 localectl set-x11-keymap fr,us '' bepo_afnor,
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
